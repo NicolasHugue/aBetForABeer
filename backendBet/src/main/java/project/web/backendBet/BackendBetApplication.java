d2ea06c5a1d70866eac727379ab53c75efdefb90
Nicolas <hugue.nicolas@gmail.com>
2025-08-08 18:20:00 +0200

    BE -Create spring project

---
+++ backendBet/src/main/java/project/web/backendBet/BackendBetApplication.java
@@ -0,0 +1,13 @@
+package project.web.backendBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBetApplication.class, args);
+	}
+
+}
